aa3c8bdf396f9610e784fcf87c2f6a02d9a6987e
Lucas Rodrigues <lucasrodrigues22020@gmail.com>
2023-01-25 01:35:22 -0300

    deploy

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 mongoose.set('strictQuery', true);
 
 async function main(){
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.inxpxo8.mongodb.net/?retryWrites=true&w=majority`)
+        await mongoose.connect(process.env.MONGO_CON)
         console.log('Conectado com sucesso!')
 }
 
